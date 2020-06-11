1ecdf75dc706a373d4314a4ba643aaa45f707ef8
Thomas Stapleton <tst@tradeshift.com>
2020-06-10 22:24:53 -0700

    allow archiving of bugs

--- src/cards.js
+++ src/cards.js
@@ -5,7 +5,7 @@ const store = storeService.getStore();
 function archiveIssues() {
 	const issues = store.get('issues')
 		.filter((issue) => issue.printStatus === 'TODO')
-		.filter((issue) => issue.type === 'Story')
+		.filter((issue) => issue.type === 'Story' || issue.type === 'Bug')
 		.write();
 	issues.map((issue) => store.get('issues').set(issue.key, Object.assign({}, issue, { printStatus: 'ARCHIVED' })).write());
 	return issues.length;
